b042954b00f1f614bf4e723eb8d4d2d5e2423ecf
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-02-02 18:08:59 -0700

    Test: movie facade complete

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 RSpec.describe MovieFacade do
   before(:each) do
+
     @facade = MovieFacade.new
     json_top_rated_page_1 = File.read('./spec/fixtures/top_rated_page_1.json')
     json_movie_search_john_wick = File.read('./spec/fixtures/movie_search_john_wick.json')
+    json_movie_2 = File.read('./spec/fixtures/movie_2.json')
 
     stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&language=en-US&page=1").
          to_return(status: 200, body: json_top_rated_page_1, headers: {})
     stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_api_key']}&include_adult=false&language=en-US&page=1&query=John%20Wick").
          to_return(status: 200, body: json_movie_search_john_wick, headers: {})
-
+    stub_request(:get, "https://api.themoviedb.org/3/movie/2?api_key=#{ENV['movie_api_key']}&language=en-US").
+         to_return(status: 200, body: json_movie_2, headers: {})
   end
+
   describe '#get_movie_by_id' do
     it 'returns the correct movie' do
       movie = @facade.get_movie_by_id(2)
